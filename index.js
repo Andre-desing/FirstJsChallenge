776b1b1ef3cf1ddf99c2c0df67436293985b48e9
Andrés Miranda <andres.miranda_@outlook.com>
2021-06-11 16:46:37 -0500

    Adding the same excercise but with a function instead

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var base = 8;
-var height = 5;
-const division = 2;
-var calculation = (base * height / division);
+// var base = 8;
+// var height = 5;
+// const division = 2;
+// var calculation = (base * height / division);
 
-console.log (calculation)
+// console.log (calculation)
+
+function calculation (base, height) {
+    
+    console.log (base * height /2)
+}
+
+calculation (8, 5);
