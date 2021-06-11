eb7f05b94cd512f266eed797d6f4f6446bb4c14d
Andrés Miranda <andres.miranda_@outlook.com>
2021-06-10 22:19:45 -0500

    adding another variable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var base = 8;
 var height = 5;
-const division = 2; 
+const division = 2;
+var calculation = (base * height / division);
 
-console.log (base * height / division)
+console.log (calculation)
